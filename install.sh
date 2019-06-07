0a2d521e848d629f7031ad65a217cc7fe7d9cd53
Verdin, Alexandre <48286713+nidrev@users.noreply.github.com>
2019-06-07 09:16:46 -0300

    Update install.sh
    Atualização para o SpedFiscal

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 clear
 
-SPED_VERSION="SPEDContabil_linux_x86-6.0.5.jar"
+SPED_VERSION="SpedEcf_linux_x64-5.1.1.jar"
 
 
 echo "Atualizando sistema..."
@@ -15,17 +15,17 @@ sudo apt-get install -y libc6:i386 libncurses5:i386 libstdc++6:i386 libxi6:i386 
 sudo apt-get install ttf-mscorefonts-installer
 sudo fc-cache
 
-wget http://www.receita.fazenda.gov.br/publico/programas/Sped/SpedContabil/${SPED_VERSION}
+wget http://www.receita.fazenda.gov.br/publico/programas/Sped/ECF/${SPED_VERSION}
 
 tar -zxvf jre.tar.gz
 ./jre/bin/java -jar ${SPED_VERSION}
-rm -rf  $HOME/ProgramasSPED/SpedContabil/jre/
+rm -rf  $HOME/ProgramasSPED/SpedFiscal/jre/
 rm -f ${SPED_VERSION}
 echo "Movendo JRE..."
-mv jre $HOME/ProgramasSPED/SpedContabil
+mv jre $HOME/ProgramasSPED/SpedFiscal
 echo "Permissão de arquivos..."
-sudo chmod -Rf 777 $HOME/ProgramasSPED/SpedContabil/contabilpva.jar
-sudo chmod -Rf 777 $HOME/ProgramasSPED/SpedContabil/jre/
+sudo chmod -Rf 777 $HOME/ProgramasSPED/SpedFiscal/contabilpva.jar
+sudo chmod -Rf 777 $HOME/ProgramasSPED/SpedFiscal/jre/
 echo '-------------------'
 echo '\0/ Instalado! \0/'
 echo '-------------------'
